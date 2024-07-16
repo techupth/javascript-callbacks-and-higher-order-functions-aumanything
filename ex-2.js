5a3c7c51aa3f0660a8664f6eb8b33626e65d10f6
thiranattada <thiranattada.n@muze.co.th>
2024-07-16 15:06:23 +0700

    submit assignment

--- ex-2.js
+++ ex-2.js
@@ -2,11 +2,20 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  for (let i = 0, i < array.lenght; i ++)
+  {
+    let n;    
+    if array[i] > 70 ? n = n + 1 : n = n;
+  }
+    return n
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
+
+atleaseFive(studentScoresRoom1, )
+
 
 // Using `atLeastFive` function here
 
